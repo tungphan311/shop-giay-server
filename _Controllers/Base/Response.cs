116e681fe92a8de658a611016c8de6be86330e2a
vinhpham14 <qvinhpham98@gmail.com>
2020-05-21 21:23:08 +0700

    fix references loop

--- _Controllers/Base/Response.cs
+++ _Controllers/Base/Response.cs
@@ -10,19 +10,20 @@ namespace shop_giay_server._Controllers
         public string Code { get; set; }
         public string Msg { get; set; }
         public int Total { get; set; }
-        // public List<ItemType> Data { get; set; }
+        // public List<ItemType> DataJson { get; set; }
         public string Data { get; set; }
 
         public Response(IEnumerable<ItemType> data, string code = "OK", string msg = "Success.")
         {
-            // ReferenceLoopHandling.Ignore
             var returnData = data.ToList();
             var convertSetting = new JsonSerializerSettings
             {
-                PreserveReferencesHandling = PreserveReferencesHandling.Objects
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                MaxDepth = 3,
             };
 
-            Data = JsonConvert.SerializeObject(returnData, Formatting.Indented, convertSetting);
+            // DataJson = returnData;
+            Data = JsonConvert.SerializeObject(returnData, Formatting.None, convertSetting);
             Total = data.Count();
             Code = code;
             Msg = msg;
