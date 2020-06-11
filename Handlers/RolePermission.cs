827a82dbf782ed1925adf4469e41338ecffdd2fa
tungphan311 <thanhtunga1lqd@gmail.com>
2020-06-11 20:59:51 +0700

    set admin all permission

--- Handlers/RolePermission.cs
+++ Handlers/RolePermission.cs
@@ -1,11 +1,31 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
 
 namespace shop_giay_server.Handlers
 {
     public static class RolePermission
     {
-        public static readonly List<string> admin = new List<string>() { PermissionConstant.Create_User, PermissionConstant.Shoes };
-        public static readonly List<string> staff = new List<string>() { };
+        public static List<string> admin = getAllPermissions();
+        public static List<string> staff = new List<string>() {
+            PermissionConstant.Create_Shoes, PermissionConstant.Shoes, PermissionConstant.Update_Shoes, PermissionConstant.Delete_Shoes
+        };
+
+        public static List<string> getAllPermissions()
+        {
+            List<string> permissions = new List<string>();
+
+            Type t = typeof(PermissionConstant);
+            FieldInfo[] fields = t.GetFields(BindingFlags.Static | BindingFlags.Public);
+
+            foreach (var field in fields)
+            {
+                permissions.Add(field.GetValue(null).ToString());
+            }
+
+            return permissions;
+        }
     }
 }
