d1b437ac38f3d56499c053b3cc1f93ee26a5fdce
tungphan311 <thanhtunga1lqd@gmail.com>
2020-07-01 22:35:26 +0700

    deploy fe

---
+++ wwwroot/precache-manifest.6d8d6a7c7a4d8b5f79f6c20d55a86f61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46967849fe7ffa4ee6bde2a6d0b1609a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd9be290d73d67398f5c",
+    "url": "/static/css/2.18d5bb7d.chunk.css"
+  },
+  {
+    "revision": "c38cc1837e7dad2397bb",
+    "url": "/static/css/main.d9d1290e.chunk.css"
+  },
+  {
+    "revision": "bd9be290d73d67398f5c",
+    "url": "/static/js/2.2dfc30cd.chunk.js"
+  },
+  {
+    "revision": "83fc6998b5694c722f3a165021eeeaef",
+    "url": "/static/js/2.2dfc30cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38cc1837e7dad2397bb",
+    "url": "/static/js/main.064a435b.chunk.js"
+  },
+  {
+    "revision": "79b5342668e1e615d170",
+    "url": "/static/js/runtime-main.25b471c0.js"
+  }
+]);
